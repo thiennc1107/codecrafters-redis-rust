5cdae358fee581cb4628a1f6acdf7febef78a20f
ThieNguyen <thiennc1107@gmail.com>
2024-04-09 23:13:48 +0700

    pass 2st stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::{Error, Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,11 +8,27 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+               match handle_client(_stream) {
+                    Ok(..)=> {
+                        print!("handle client succesfully");
+                    }
+                    Err(e) => {
+                        print!("failed to handle client err={e}")
+                    }
+
+               };
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error>{
+    // let mut buf = [0; 512];
+    loop {
+        // let bytes_read = stream.read(&mut  buf)?;
+        stream.write_all("pong".as_bytes())?;
     }
 }
